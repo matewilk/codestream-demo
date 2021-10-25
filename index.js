c234ce7fc7ec8d7685778e7efb91b106c5b6fa37
Mat Wilk <matewilk@gmail.com>
2021-10-25 12:52:50 +0100

    simple express server and nr integration

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('code-stream-test');
+const newrelic = require("newrelic");
+const express = require("express");
+const parse = require("url").parse;
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/custom', (req, res) => {
+  const parsedUrl = parse(req.url, true);
+  const { pathname } = parsedUrl;
+
+  newrelic.setTransactionName(pathname);
+
+  res.send(pathname);
+});
+
+app.get('/error', (req, res) => {
+  const test = abc('');
+
+  res.send(test);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
